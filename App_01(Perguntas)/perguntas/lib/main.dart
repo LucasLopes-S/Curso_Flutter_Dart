f9528cad3d62e13588ccf0a225ff4ee87087b854
Lucas Lopes de Sousa <lucaslopes150995@gmail.com>
2021-03-02 17:41:00 -0300

    Aula 32
    Integração de classes com o botão

--- App_01(Perguntas)/perguntas/lib/main.dart
+++ App_01(Perguntas)/perguntas/lib/main.dart
@@ -1,17 +1,41 @@
 import 'package:flutter/material.dart';
 
-main() =>runApp(new PerguntaApp());
-
+main() => runApp(new PerguntaApp());
 
 class PerguntaApp extends StatelessWidget {
+  void resposta() {
+    print('Pergunta Respondida');
+  }
+
   @override
   Widget build(BuildContext context) {
+    final perguntas = [
+      'Qual é a sua cor favorita?',
+      'Qual é o seu animal favorito?'
+    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Perguntas'),
         ),
-        body: Text("Texto aqui!"),
+        body: Column(
+          children: [
+            Text(perguntas[0]),
+            RaisedButton(
+              child: Text('Resposta 1'),
+              onPressed: resposta,
+            ),
+            RaisedButton(
+              child: Text('Resposta 2'),
+              onPressed: resposta,
+            ),
+            RaisedButton(
+              child: Text('Resposta 3'),
+              onPressed: resposta,
+            ),
+          ],
+        ),
       ),
     );
   }
