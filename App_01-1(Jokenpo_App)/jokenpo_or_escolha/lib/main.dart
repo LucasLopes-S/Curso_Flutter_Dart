bb3ff926a46151636565d557f19b60288e7a75c2
Lucas Lopes de Sousa <lucaslopes150995@gmail.com>
2021-03-04 03:13:19 -0300

    Organização de pastas antes do desenvolvimento
    Somente organização da nomenclatura das pastas que utilizando de caracteres especiais estava gerando erro no deploy

---
+++ App_01-1(Jokenpo_App)/jokenpo_or_escolha/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+main() {
+  runApp(Jokenpo());
+}
+
+class Jokenpo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Text('Inicio do desenvolvimento!!'),
+    );
+  }
+}
